57f066e198e43530151774ea806c44f10bd94ac2
Eric Schles <ericschles@gmail.com>
2015-11-18 11:21:41 -0500

    can upload pdfs

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "/home/eric/Documents/whitehouse/VAUSDS/personal_projects/pdf_to_web/pdfs"
+
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,20 +1,36 @@
 from app import app
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for
 import os
+from werkzeug import secure_filename
+
+ALLOWED_EXTENSIONS = set(['pdf'])
 
 #This route will be the splash page for the tool, it will have log in eventually. Right now it will just have header's and footers
 @app.route("/",methods=["GET","POST"])
 def index():
     return render_template("index.html")
 
-@app.route("/ingestion",methods=["GET","POST"])
-def ingestion():
-    if request.method == "POST":
-        #File upload goes here
-        #Also allow for folder ingestion
-        return render_template("ingestion.html") # consider changing this
-    else:
-        return render_template("ingestion.html")
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/ingestion', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "success"
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 @app.route("/viewer",methods=["GET","POST"])
 def viewer():
